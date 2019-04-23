8391d206c8d61dbc4aa1a9cc9fe3f63c9c204589
josh182014 <josh182014@gmail.com>
2019-04-23 17:28:33 -0600

    changed a class to className

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Display.css';
 
 const CalculatorDisplay = props => {
-    return <div class='display'>{props.text}</div>
+    return <div className='display'>{props.text}</div>
 };
 
 export default CalculatorDisplay;
